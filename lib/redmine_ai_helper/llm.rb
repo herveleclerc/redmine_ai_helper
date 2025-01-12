7e0895cff9e314fdd68ab3d7ba3a5af20c276f7a
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-01-12 15:38:35 +0000

    setting constructor

--- lib/redmine_ai_helper/llm.rb
+++ lib/redmine_ai_helper/llm.rb
@@ -1,8 +1,15 @@
 require 'openai'
 module RedmineAiHelper
+
+  BASE_URL = 'https://api.openai.com/v1'.freeze
   class Llm
     def initialize(params = {})
-      @client = OpenAI::Client.new(access_token: "access_token_goes_here")
+      params[:access_token] ||= Setting.plugin_redmine_ai_helper['access_token']
+      params[:uri_base] ||= Settings.plugin_redmine_ai_helper['uri_base']
+      params[:organization_id] ||= Setting.plugin_redmine_ai_helper['organization_id']
+
+      params[:uri_base] = BASE_URL if params[:uri_base].blank?
+      @client = OpenAI::Client.new(params)
     end
   end
 end
