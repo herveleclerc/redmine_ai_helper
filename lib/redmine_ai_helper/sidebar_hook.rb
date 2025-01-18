6a6c3ae823be670564d62a9edb14f46077f34161
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-01-18 01:59:02 +0000

    fix filename error

--- lib/redmine_ai_helper/sidebar_hook.rb
+++ lib/redmine_ai_helper/sidebar_hook.rb
@@ -1,7 +1,5 @@
 module RedmineAiHelper
-  class << self
     class SidebarHook < Redmine::Hook::ViewListener
       render_on :view_issues_sidebar_planning_bottom, partial: 'ai_helper/sidebar'
     end
-  end
 end
