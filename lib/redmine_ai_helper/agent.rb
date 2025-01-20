ce9cac4701eef9b5504371cdb96d2e2944cf624e
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-01-20 12:16:12 +0000

    fix query generator

--- lib/redmine_ai_helper/agent.rb
+++ lib/redmine_ai_helper/agent.rb
@@ -346,71 +346,94 @@ module RedmineAiHelper
     end
 
     # フィルター条件からIssueを検索するためのURLを生成する
+    #           {
+    #   name: "generate_issue_search_url",
+    #   description: "Generate a URL for searching issues based on the filter conditions. For search items with '_id', specify the ID instead of the name of the search target. If you do not know the ID, you need to call capable_issue_properties in advance to obtain the ID.",
+    #   arguments: {
+    #     schema: {
+    #       type: "object",
+    #       properties: {
+    #         project_id: "integer",
+    #         fields: {
+    #           type: "array",
+    #           items: {
+    #             type: "object",
+    #             properties: {
+    #               field_name: {
+    #                 type: "string",
+    #                 enum: ["tracker_id", "status_id", "priority_id", "category_id", "version_id", "created_on", "updated_on", "subject", "description", "assigned_to_id", "author_id", "start_date", "due_date", "done_ratio", "estimated_hours", "total_estimated_hours", "spent_hours", "total_spent_hours"],
+    #               },
+    #               operator: {
+    #                 type: "string",
+    #                 enum: ["==", "!=", ">", "<", ">=", "<=", "*", "!*", "o", "!o", "c", "!c"],
+    #                 description: "Operators: == (equal), != (not equal), > (greater than), < (less than), >= (greater than or equal), <= (less than or equal), * (contains), !* (does not contain), o (open), !o (closed), c (is), !c (is not)",
+    #               },
+    #               value: {
+    #                 "anyOf": [
+    #                   { type: "string" },
+    #                   { type: "array", items: { type: "string" } },
+    #                 ],
+    #               },
+    #             },
+    #             required: ["field_name", "operator", "value"],
+    #           },
+    #         },
+    #         custom_fields: {
+    #           type: "array",
+    #           items: {
+    #             type: "object",
+    #             properties: {
+    #               field_id: "integer",
+    #               operator: {
+    #                 type: "string",
+    #                 enum: ["==", "!=", ">", "<", ">=", "<=", "*", "!*", "o", "!o", "c", "!c"],
+    #                 description: "Operators: == (equal), != (not equal), > (greater than), < (less than), >= (greater than or equal), <= (less than or equal), * (contains), !* (does not contain), o (open), !o (closed), c (is), !c (is not)",
+    #               },
+    #               value: "string",
+    #             },
+    #             required: ["field_id", "operator", "value"],
+    #           },
+    #         },
+    #       },
+    #       required: ["project_id", "fields", "custom_fields"],
+    #       is_open: "boolean",
+    #     },
+    #   },
+    # }
     def generate_issue_search_url(args = {})
-
-      #
-      # filterの例：
-      # {
-      #  project_id: 1,
-      #  fields: [
-      #   {
-      #     field_name: "tracker_id",
-      #     operator: "==",
-      #     value: [1, 2]
-      #   },
-      #   {
-      #     field_name: "status_id",
-      #     operator: "!=",
-      #     value: [1, 2]
-      #   },
-      #   {
-      #     field_name: "created_on",
-      #     operator: ">=",
-      #     value: "2022-01-01"
-      #    },
-      #    {
-      #     field_name: "subject",
-      #     operator: "*",
-      #     value: "foo"
-      #     },
-      #  ],
-      #  custom_fields: [
-      #   {
-      #     field_id: 1,
-      #     value: "foo"
-      #     operator: "=="
-      #    },
-      #  ]
-      #  is_open: true
-      # }
-      #
-      #
       sym_args = args.deep_symbolize_keys
       project_id = sym_args[:project_id]
       project = Project.find(project_id)
       fields = sym_args[:fields]
       custom_fields = sym_args[:custom_fields]
       url = "/projects/#{project}/issues?"
-      # url += "utf8=%E2%9C%93&set_filter=1&f[]=project_id&op[project_id]=%3D&v[project_id][]=#{project_id}"
       url += "utf8=%E2%9C%93&set_filter=1"
+
+      field_params = {}
       fields.each do |field|
         field_name = field[:field_name]
         operator = field[:operator]
         value = field[:value]
-        if value.is_a?(Array)
-          value.each do |v|
-            url += "&f[]=#{field_name}&op[#{field_name}]=#{operator}&v[#{field_name}][]=#{v}"
-          end
-        else
-          url += "&f[]=#{field_name}&op[#{field_name}]=#{operator}&v[#{field_name}]=#{value}"
+        field_params[field_name] ||= { operator: operator, values: [] }
+        field_params[field_name][:values] += Array(value)
+      end
+
+      field_params.each do |field_name, params|
+        operator = params[:operator]
+        values = params[:values]
+        url += "&f[]=#{field_name}&op[#{field_name}]=#{operator}"
+        values.each do |v|
+          url += "&v[#{field_name}][]=#{v}"
         end
       end
+
       custom_fields.each do |field|
         field_id = field[:field_id]
         operator = field[:operator]
         value = field[:value]
         url += "&f[]=cf_#{field_id}&op[cf_#{field_id}]=#{operator}&v[cf_#{field_id}]=#{value}"
       end
+
       url += "&f[]=status_id&op[status_id]=o&v[status_id][]=open" if sym_args[:is_open]
 
       return { url: url }
