da74c41b9d4688cbf71e0ef52a55d1874ad0f597
Haru Iida <haru.ii.a9i1e@gmail.com>
2025-01-29 01:38:28 +0000

    add activities list

--- lib/redmine_ai_helper/agents/project_agent.rb
+++ lib/redmine_ai_helper/agents/project_agent.rb
@@ -1,8 +1,10 @@
 require "redmine_ai_helper/base_agent"
+require "redmine_ai_helper/logger"
 
 module RedmineAiHelper
   module Agents
     class ProjectAgent < RedmineAiHelper::BaseAgent
+      include RedmineAiHelper::Logger
       RedmineAiHelper::BaseAgent.add_agent(name: "project_agent", class: self)
       def self.list_tools()
         list = {
@@ -57,6 +59,39 @@ module RedmineAiHelper
                 },
               },
             },
+            {
+              name: "list_project_activities",
+              description: "List all activities of the project.",
+              arguments: {
+                schema: {
+                  type: "object",
+                  properties: {
+                    project_id: "integer",
+                    author_id: {
+                      type: "integer",
+                      description: "The user ID of the author of the activity. If not specified, it will return all activities.",
+                    },
+                    limit: {
+                      type: "integer",
+                      description: "The maximum number of activities to return. If not specified, it will return all activities.",
+                      default: 100,
+                    },
+                    start_date: {
+                      type: "string",
+                      format: "date",
+                      description: "The start date of the activities to return.",
+                      default: "30 Days Ago",
+                    },
+                    end_date: {
+                      type: "string",
+                      format: "date",
+                      description: "The end date of the activities to return. If not specified, it will return all activities.",
+                    },
+                    required: ["project_id"],
+                  }
+                },
+              },
+            }
           ],
         }
         list
@@ -158,6 +193,42 @@ module RedmineAiHelper
           project_id: project_id,
           enabled_modules: enabled_modules,
         }
+        AgentResponse.create_success json
+      end
+
+      # List all activities of the project.
+      def list_project_activities(args = {})
+        sym_args = args.deep_symbolize_keys
+        project_id = sym_args[:project_id]
+        project = Project.find(project_id)
+        author_id = sym_args[:author_id]
+        author = author_id ? User.find(author_id) : nil
+        limit = sym_args[:limit] || 100
+        start_date = sym_args[:start_date] || 30.days.ago
+        end_date = sym_args[:end_date] || 1.day.from_now
+
+        current_user = User.current
+        fetcher = Redmine::Activity::Fetcher.new(
+          current_user, 
+          project: project,
+          author: author,
+        )
+        ai_helper_logger.info "current_user: #{current_user}, project: #{project}, author: #{author}, start_date: #{start_date}, end_date: #{end_date}, limit: #{limit}"
+        events = fetcher.events(start_date, end_date).sort_by(&:event_datetime).reverse.first(limit)
+        # events = fetcher.events(start_date, end_date, limit).sort_by(&:event_datetime)
+        # events = fetcher.events(start_date)
+        list = []
+        events.each do |event|
+          list << {
+            id: event.id,
+            event_datetime: event.event_datetime,
+            event_type: event.event_type,
+            event_title: event.event_title,
+            event_description: event.event_description,
+            event_url: event.event_url,
+          }
+        end
+        json = {"activities": list}
         AgentResponse.create_success json
       end
     end
