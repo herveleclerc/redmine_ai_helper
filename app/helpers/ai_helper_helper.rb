e91c6fd9040d9c303b5ddd01bc27da8bd620a168
Haru Iida <haru.ii.a9i1e@gmail.com>
2025-02-03 13:25:43 +0000

    Change Commonmark to helper

--- app/helpers/ai_helper_helper.rb
+++ app/helpers/ai_helper_helper.rb
@@ -1,2 +1,9 @@
 module AiHelperHelper
+  def md_to_html(text)
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SanitizationFilter,
+    ]
+    pipeline.call(text)[:output].to_s.html_safe
+  end
 end
