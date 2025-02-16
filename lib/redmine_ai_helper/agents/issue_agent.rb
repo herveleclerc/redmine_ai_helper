feefb7313a4c08c5dc3780023f1b1419b9583281
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-02-16 09:33:06 +0000

    add project tool to issu agent

--- lib/redmine_ai_helper/agents/issue_agent.rb
+++ lib/redmine_ai_helper/agents/issue_agent.rb
@@ -5,12 +5,13 @@ module RedmineAiHelper
       def backstory
         content = <<~EOS
           あなたは RedmineAIHelper プラグインのチケットエージェントです。Redmine のチケットに関する問い合わせに答えます。
+          なお、「条件に合ったチケットを探して」「こういう条件のチケット見せて」の様なたくさんのチケット探すタスクの場合には、検索条件を指定したチケット一覧のリンクを返してください。
         EOS
         content
       end
 
       def available_tool_providers
-        ["issue_tool_provider"]
+        ["issue_tool_provider", "project_tool_provider"]
       end
     end
   end
