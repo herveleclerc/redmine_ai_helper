feb314f387cb1cc399d97bd18a82820ac4dc643b
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-02-23 08:10:42 +0000

    fix wrong directory name

--- .github/build-scripts/env.sh
+++ .github/build-scripts/env.sh
@@ -1,5 +1,6 @@
 GITHUB_DIR=$(dirname $(pwd))
 export PATH_TO_PLUGIN=`dirname ${GITHUB_DIR}`
-export TESTSPACE=$PATH_TO_PLUGIN/testspace
+export TESTSPACE_NAME=testspace
+export TESTSPACE=$PATH_TO_PLUGIN/$TESTSPACE_NAME
 export PATH_TO_REDMINE=$TESTSPACE/redmine
 export RAILS_ENV=test
