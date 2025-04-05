819a08345be299f9313a9003659976f736036660
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-04-05 08:08:58 +0000

    controller and model generated

---
+++ app/controllers/ai_helper_settings_controller.rb
@@ -0,0 +1,11 @@
+class AiHelperSettingsController < ApplicationController
+
+  def index
+  end
+
+  def update
+  end
+
+  def new_model_profile
+  end
+end
---
+++ app/models/ai_helper_model_profile.rb
@@ -0,0 +1,2 @@
+class AiHelperModelProfile < ApplicationRecord
+end
