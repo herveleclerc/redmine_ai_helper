bb55d5e3033b4831f4b689645188c7505bc2eae3
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-04-12 07:24:25 +0000

    add OpenAI API Compatibility

--- app/models/ai_helper_model_profile.rb
+++ app/models/ai_helper_model_profile.rb
@@ -2,20 +2,31 @@ class AiHelperModelProfile < ApplicationRecord
   include Redmine::SafeAttributes
   validates :name, presence: true, uniqueness: true
   validates :llm_type, presence: true
-  validates :access_key, presence: true
+  validates :access_key, presence: true, if: :access_key_required?
   validates :llm_model, presence: true
+  validates :base_uri, presence: true, if: :base_uri_required?
+  validates :base_uri, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }, if: :base_uri_required?
 
-  safe_attributes 'name', 'llm_type', 'access_key', 'organization_id', 'base_uri', 'version', 'llm_model'
+  safe_attributes "name", "llm_type", "access_key", "organization_id", "base_uri", "version", "llm_model"
 
   # Replace all characters after the 4th with *
   def masked_access_key
     return access_key if access_key.blank? || access_key.length <= 4
     masked_key = access_key.dup
-    masked_key[4..-1] = '*' * (masked_key.length - 4)
+    masked_key[4..-1] = "*" * (masked_key.length - 4)
     masked_key
   end
 
   def display_name
     "#{name} (#{llm_type}: #{llm_model})"
+  end
+
+  def base_uri_required?
+    # Check if the llm_type is OpenAICompatible or Gemini
+    llm_type == RedmineAiHelper::LlmProvider::LLM_OPENAI_COMPATIBLE
+  end
+
+  def access_key_required?
+    llm_type != RedmineAiHelper::LlmProvider::LLM_OPENAI_COMPATIBLE
   end
 end
