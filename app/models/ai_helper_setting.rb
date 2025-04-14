cb8af56fed49c515f3e6272108e5055cfbf3f6d1
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-04-14 22:51:42 +0000

    add vector search setting

--- app/models/ai_helper_setting.rb
+++ app/models/ai_helper_setting.rb
@@ -1,8 +1,10 @@
 class AiHelperSetting < ApplicationRecord
   include Redmine::SafeAttributes
   belongs_to :model_profile, class_name: "AiHelperModelProfile"
+  validates :vector_search_uri, :presence => true, if: :vector_search_enabled?
+  validates :vector_search_uri, :format => { with: URI::regexp(%w[http https]), message: l("ai_helper.model_profiles.messages.must_be_valid_url") }, if: :vector_search_enabled?
 
-  safe_attributes 'model_profile_id', 'additional_instructions'
+  safe_attributes "model_profile_id", "additional_instructions", "version", "vector_search_enabled", "vector_search_uri", "vector_search_api_key"
 
   def AiHelperSetting::find_or_create
     setting = AiHelperSetting.order(:id).first
