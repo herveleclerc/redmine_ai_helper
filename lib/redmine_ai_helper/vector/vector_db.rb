67dc2c9efa34716df4c6fc37e7734835a628bb83
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-04-15 22:49:30 +0000

    add vector_data model

--- lib/redmine_ai_helper/vector/vector_db.rb
+++ lib/redmine_ai_helper/vector/vector_db.rb
@@ -40,25 +40,34 @@ module RedmineAiHelper
         rescue Faraday::ResourceNotFound
           # do nothing
         end
+        AiHelperVectorData.where(index: index_name).destroy_all
       end
 
       def add_datas(datas:)
         texts = []
         uuids = []
+        vector_datas = []
         return if datas.empty?
         datas.each do |data|
+          next if AiHelperVectorData.exists?(object_id: data.id, index: index_name)
           text = data_to_jsontext(data)
           uuid = SecureRandom.uuid
           texts << text
           uuids << uuid
+          vector_datas << AiHelperVectorData.new(object_id: data.id, index: index_name, uuid: uuid)
           print "."
           if texts.size >= 100
             client.add_texts(texts: texts, ids: uuids)
             texts = []
-            uuids = []
           end
+          vector_datas.each { |v| v.save! }
+
+          vector_data = []
         end
-        client.add_texts(texts: texts, ids: uuids)
+        unless texts.empty?
+          client.add_texts(texts: texts, ids: uuids)
+          vector_datas.each { |v| v.save! }
+        end
         puts ""
       end
 
