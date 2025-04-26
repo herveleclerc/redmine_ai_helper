f941867fdd845e4e20d74b22bc94e9d1481d38c5
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-04-26 13:15:34 +0000

    Translate prompts to English

--- lib/redmine_ai_helper/llm_client/open_ai_compatible_provider.rb
+++ lib/redmine_ai_helper/llm_client/open_ai_compatible_provider.rb
@@ -70,22 +70,22 @@ module RedmineAiHelper
                   properties: {
                     id: {
                       type: "string",
-                      description: "ツールのID", # TODO: 英語にする
+                      description: "The ID of the tool",
                     },
                     type: {
                       type: "string",
-                      description: "function 固定", # TODO: 英語にする
+                      description: "Fixed to 'function'",
                     },
                     function: {
                       type: "object",
                       properties: {
                         name: {
                           type: "string",
-                          description: "ツールの名前", # TODO: 英語にする
+                          description: "The name of the tool",
                         },
                         arguments: {
                           type: "string",
-                          description: "ツールの引数のJSON文字列表現", # TODO: 英語にする
+                          description: "JSON string representation of the tool's arguments",
                         },
                       },
                       required: ["name", "arguments"],
@@ -98,16 +98,12 @@ module RedmineAiHelper
             required: ["tool_calls"],
           }
           parser = Langchain::OutputParsers::StructuredOutputParser.from_json_schema(json_schema)
-          # TODO: 英語にする
           prompt = <<~EOS
-            ユーザーのメッセージを解析して、ユーザの要求を解決するために最適なツールを選択してください。
-
-            答えはJSON形式で返してください。
-            ** JSONのみを返してください。解説は不要です。**
+            Analyze the user's message and select the most appropriate tools to resolve the user's request.
 
             #{parser.get_format_instructions}
 
-            JSONの例:
+            JSON example:
             {
               "tool_calls": [
                 {
@@ -122,14 +118,14 @@ module RedmineAiHelper
               ]
             }
             ----
-            ツールのリスト(JSON形式):
+            List of tools (JSON format):
             #{JSON.pretty_generate(tools)}}
           EOS
 
           new_messages = messages.dup
-          # TODO: 英語にする
+
           # Some LLMs may throw an error if user messages are consecutive, so insert an assistant message in between.
-          new_messages << Langchain::Assistant::Messages::OpenAIMessage.new(role: "assistant", content: "お任せください").to_hash
+          Langchain::Assistant::Messages::OpenAIMessage.new(role: "assistant", content: "ok").to_hash
           new_messages << Langchain::Assistant::Messages::OpenAIMessage.new(role: "user", content: prompt).to_hash
           response = chat(messages: new_messages)
 
@@ -173,22 +169,20 @@ module RedmineAiHelper
               next
             end
             if message[:tool_calls].is_a?(Array)
-              # TODO: 英語にする
               prompt = <<~EOS
-                回答を作成するために、以下のツールを実行して結果を教えてください。結果に基づいて回答を作成します。
+                To create a response, please execute the following tools and provide the results. Based on the results, I will create a response.
                 tool:
-                #{message[:tool_calls].to_json}
+                #{message[:tool_calls].to_json}}
               EOS
               new_messages << Langchain::Assistant::Messages::OpenAIMessage.new(role: "assistant", content: prompt).to_hash
               next
             end
           end
 
-          # TODO: 英語にする
           prompt = <<~EOS
-            あなたの指定したツールを実行したところ、以下の実行結果が得られました。この結果を元に、あなたの最終的な回答をしてください。
+            The tools you specified have been executed, and the following results were obtained. Based on these results, please provide your final response.
 
-            ツールの実行結果(JSON形式):
+            Tool execution results (in JSON format):
             #{JSON.pretty_generate(tool_results)}
           EOS
 
