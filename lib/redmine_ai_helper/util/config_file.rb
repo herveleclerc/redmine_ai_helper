0d271ff2e126959eed9395e1fd641ed04118aeae
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-05-15 13:49:02 +0000

    fix recursive locking error.

--- lib/redmine_ai_helper/util/config_file.rb
+++ lib/redmine_ai_helper/util/config_file.rb
@@ -4,7 +4,6 @@ module RedmineAiHelper
       def self.load_config
         config_file_path = Rails.root.join("config", "ai_helper", "config.yml")
         unless File.exist?(config_file_path)
-          RedmineAiHelper::CustomLogger.instance.warn("Config file not found: #{config_file_path}")
           return {}
         end
 
